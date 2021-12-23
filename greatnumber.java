edf2a22c38365c3d89d27f924da55c84b3542d7f
Vashim-Ahmad <moontoon3dnetwork@gmail.com>
2021-12-23 19:34:55 +0530

    Second Commit

---
+++ greatnumber.java
@@ -0,0 +1,22 @@
+
+public class greatnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=60, b=90, c=40;  
+		
+		if(a>=b && a>=c)  
+		System.out.println(a+" is the Greatest Number");  
+		 
+		else if (b>=a && b>=c)  
+			
+		System.out.println(b+" is the Greatest Number");  
+		else  
+		
+		System.out.println(c+" is the Greatest number");  
+	
+	
+
+	}
+
+}
